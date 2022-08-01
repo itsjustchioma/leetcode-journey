863ca28bc742a8702c3e85b0c14a8503ac0a5129
itsjustchioma <74658446+itsjustchioma@users.noreply.github.com>
2022-08-01 20:38:04 +0100

    Update unique-paths.js

--- unique-paths.js
+++ unique-paths.js
@@ -1,4 +1,4 @@
-> There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+ There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
 
 Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
 
@@ -27,10 +27,11 @@ Constraints:
 
 SOLUTION
 let uniquePaths = function(m, n) {
-    if (m == 1 || n == 1)
-        return 1;
-        
-        return uniquePaths(m-1, n)+ uniquePaths(m, n-1);
+   let path = 1;
     
-    console.log(uniquePaths(3, 7));
+    for(let i > n; i < m + n - 1; i++){
+        paths *= i;
+        paths /= (i - n + 1);
+    }
+    return paths;
 };
